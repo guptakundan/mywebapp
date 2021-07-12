af0f06dc4739f323d1e8dadfd1247d84fced0a7b
guptakundan <tanuj_kundan@hotmail.com>
2021-07-12 16:59:16 +0530

    Fixed AB#5

--- MyWebApp/Program.cs
+++ MyWebApp/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-
+//fixed
 namespace MyWebApp
 {
 	public class Program
